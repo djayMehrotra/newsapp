5642b85f3f8f1c6607c84c38ddee05420c372593
djayMehrotra <mehrotradj23@gmail.com>
2021-10-12 10:55:10 +0530

    #39 with errror in App.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,37 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, sourceName } = this.props;
-        return (
+const NewsItem = (props) => {
+    let { title, description, imageUrl, newsUrl, author, date, sourceName } = props;
+    return (
 
-            <div className="card m-2">
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    position: 'absolute',
-                    right: '0'
-                }
-                }>
+        <div className="card m-2">
+            <div style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                position: 'absolute',
+                right: '0'
+            }
+            }>
 
-                    <span className="badge rounded-pill bg-danger" style={{ left: '80%' }}>
-                        {sourceName.slice(0, 30)}
-                    </span>
-                </div>
-                <img src={imageUrl} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{title}...</h5>
-                    <p className="card-text">{description}...</p>
-                    <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
-                </div>
-                <div className="card-footer d-flex flex-column">
-                    <small className="text-muted"> <strong>Author :</strong> {author ? author : 'unknown'}</small>
-                    <small className="text-muted"> <strong>Published on :</strong> {new Date(date).toGMTString()} </small>
-                </div>
+                <span className="badge rounded-pill bg-danger" style={{ left: '80%' }}>
+                    {sourceName.slice(0, 30)}
+                </span>
             </div>
+            <img src={imageUrl} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
+            </div>
+            <div className="card-footer d-flex flex-column">
+                <small className="text-muted"> <strong>Author :</strong> {author ? author : 'unknown'}</small>
+                <small className="text-muted"> <strong>Published on :</strong> {new Date(date).toGMTString()} </small>
+            </div>
+        </div>
 
-        )
-    }
+    )
+
 }
 
 export default NewsItem
